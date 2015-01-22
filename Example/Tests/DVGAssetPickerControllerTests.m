13a10efc363c5265517fa7a8efb195fe48411630
Nikolay Morev <kolyuchiy@gmail.com>
2015-01-22 17:14:20 +0300

    Add unit test placeholder

---
+++ Example/Tests/DVGAssetPickerControllerTests.m
@@ -0,0 +1,23 @@
+//
+//  DVGAssetPickerControllerTests.m
+//  DVGAssetPickerController
+//
+//  Created by Nikolay Morev on 22.01.15.
+//  Copyright (c) 2015 DENIVIP Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DVGAssetPickerControllerTests : XCTestCase
+
+@end
+
+@implementation DVGAssetPickerControllerTests
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+@end
--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  DVGAssetPickerControllerTests.m
-//  DVGAssetPickerControllerTests
-//
-//  Created by Nikolay Morev on 01/22/2015.
-//  Copyright (c) 2014 Nikolay Morev. All rights reserved.
-//
-
-${TEST_EXAMPLE}
